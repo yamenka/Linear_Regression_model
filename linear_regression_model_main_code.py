9e67153b5e2ad40c16aa53ddc250deb26f41a0ae
yamenka <74372862+yamenka@users.noreply.github.com>
2021-09-04 16:22:16 +0200

    Update linear_regression_model_main_code.py

--- linear_regression_model_main_code.py
+++ linear_regression_model_main_code.py
@@ -6,6 +6,8 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_absolute_error
 from sklearn.metrics import mean_squared_error
 from sklearn.metrics import r2_score
+from sklearn.linear_model import Ridge
+from sklearn.linear_model import Lasso
 import numpy as np
 
 
@@ -34,3 +36,25 @@ mae = mean_absolute_error(y_test, predicted_values)
 rss = np.sum(np.square(y_test - predicted_values))
 rmse = np.sqrt(mean_squared_error(y_test, predicted_values))
 r2_score = r2_score(y_test, predicted_values)
+
+
+
+ridge_reg = Ridge(alpha=0.5)
+ridge_reg.fit(x_train, y_train)
+
+lasso_reg = Lasso(alpha = 0.001)
+lasso_reg.fit(x_train, y_train)
+
+def get_weights_df(model, feat, col_name):
+  weights = pd.Series(model.coef_, feat.columns).sort_values()
+  weights_df = pd.DataFrame(weights).reset_index()
+  weights_df.columns = ['Features', col_name]
+  weights_df[col_name].round(3)
+  return weights_df
+
+linear_model_weights = get_weights_df(linear_model, x_train, 'Linear_Model_weights')
+ridge_weights_df = get_weights_df(ridge_reg,x_train, 'Ridge_Weight' )
+lasso_weights_df = get_weights_df(lasso_reg, x_train, 'Lasso_Weight')
+
+final_weights = pd.merge(linear_model_weights, ridge_weights_df, on='Features')
+final_weights = pd.merge(final_weights, lasso_weights_df, on='Features')
